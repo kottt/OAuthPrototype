3c83de1824c6fd5dbc0ddf47895db348e7a85625
kottt <anadarion@gmail.com>
2015-11-27 18:31:35 +0300

    Identity setup (EF, Context, config)

---
+++ OAuthPrototype.API/AuthContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OAuthPrototype.API {
+	public class AuthContext : IdentityDbContext<IdentityUser> {
+		public AuthContext()
+			: base("AuthContext") {
+
+		}
+	}
+}
