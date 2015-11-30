4656d413d2ef8c46edd54c2b027b696b9cbb4b1c
kottt <anadarion@gmail.com>
2015-11-30 19:28:18 +0300

    SPA Orders service

--- OAuthPrototype.Web/app/services/ordersService.js
+++ OAuthPrototype.Web/app/services/ordersService.js
@@ -1,1 +1,18 @@
-﻿
+﻿"use strict";
+app.factory("ordersService", ["$http", function ($http) {
+
+	var serviceBase = "http://localhost:91/";
+	var ordersServiceFactory = {};
+
+	var getOrders = function () {
+
+		return $http.get(serviceBase + "api/orders").then(function (results) {
+			return results;
+		});
+	};
+
+	ordersServiceFactory.getOrders = getOrders;
+
+	return ordersServiceFactory;
+
+}]);
