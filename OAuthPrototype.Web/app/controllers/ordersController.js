da9c6d1b8d8abc75fc43fddea1f77acf843f0ed4
kottt <anadarion@gmail.com>
2015-12-01 22:35:52 +0300

    SPA RefreshTokens, Orders page

--- OAuthPrototype.Web/app/controllers/ordersController.js
+++ OAuthPrototype.Web/app/controllers/ordersController.js
@@ -1,1 +1,14 @@
-﻿
+﻿"use strict";
+app.controller("ordersController", ["$scope", "ordersService", function ($scope, ordersService) {
+
+	$scope.orders = [];
+
+	ordersService.getOrders().then(function (results) {
+
+		$scope.orders = results.data;
+
+	}, function (error) {
+		//alert(error.data.message);
+	});
+
+}]);
