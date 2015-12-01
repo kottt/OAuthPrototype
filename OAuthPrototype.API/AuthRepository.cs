bfff5dafb7815ca7f5cfcf10fdd13e5b80ae2c57
kottt <anadarion@gmail.com>
2015-12-01 14:05:13 +0300

    RefreshToken reposytory

--- OAuthPrototype.API/AuthRepository.cs
+++ OAuthPrototype.API/AuthRepository.cs
@@ -5,18 +5,20 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using OAuthPrototype.API.Entities;
 using OAuthPrototype.API.Models;
 
 namespace OAuthPrototype.API {
 	public class AuthRepository : IDisposable {
 		private readonly AuthContext _ctx;
-
 		private readonly UserManager<IdentityUser> _userManager;
 
 		public AuthRepository() {
 			_ctx = new AuthContext();
 			_userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
 		}
+
+		#region User Auth/Register
 
 		public async Task<IdentityResult> RegisterUser(UserModel userModel) {
 			IdentityUser user = new IdentityUser {
@@ -33,6 +35,56 @@ namespace OAuthPrototype.API {
 
 			return user;
 		}
+
+		#endregion
+
+		#region Refresh tokens
+
+		public Client FindClient(string clientId) {
+			var client = _ctx.Clients.Find(clientId);
+
+			return client;
+		}
+
+		public async Task<bool> AddRefreshToken(RefreshToken token) {
+			var existingToken = _ctx.RefreshTokens.SingleOrDefault(r => r.Subject == token.Subject && r.ClientId == token.ClientId);
+
+			if (existingToken != null) {
+				var result = await RemoveRefreshToken(existingToken);
+			}
+
+			_ctx.RefreshTokens.Add(token);
+
+			return await _ctx.SaveChangesAsync() > 0;
+		}
+
+		public async Task<bool> RemoveRefreshToken(string refreshTokenId) {
+			var refreshToken = await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+
+			if (refreshToken != null) {
+				_ctx.RefreshTokens.Remove(refreshToken);
+				return await _ctx.SaveChangesAsync() > 0;
+			}
+
+			return false;
+		}
+
+		public async Task<bool> RemoveRefreshToken(RefreshToken refreshToken) {
+			_ctx.RefreshTokens.Remove(refreshToken);
+			return await _ctx.SaveChangesAsync() > 0;
+		}
+
+		public async Task<RefreshToken> FindRefreshToken(string refreshTokenId) {
+			var refreshToken = await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+
+			return refreshToken;
+		}
+
+		public List<RefreshToken> GetAllRefreshTokens() {
+			return _ctx.RefreshTokens.ToList();
+		}
+
+		#endregion
 
 		public void Dispose() {
 			_ctx.Dispose();
