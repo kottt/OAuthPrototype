5f8762e407e2b579609c68d7a45902b4814a997e
kottt <anadarion@gmail.com>
2015-12-01 14:41:41 +0300

    Token generation

--- OAuthPrototype.API/Providers/SimpleAuthorizationServerProvider.cs
+++ OAuthPrototype.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -1,32 +1,13 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using OAuthPrototype.API.Entities;
 
 namespace OAuthPrototype.API.Providers {
 	public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider {
-
-		public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context) {
-
-			context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
-
-			using (AuthRepository repo = new AuthRepository()) {
-				IdentityUser user = await repo.FindUser(context.UserName, context.Password);
-
-				if (user == null) {
-					context.SetError("invalid_grant", "The user name or password is incorrect.");
-					return;
-				}
-			}
-
-			var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-			identity.AddClaim(new Claim("sub", context.UserName));
-			identity.AddClaim(new Claim("role", "user"));
-
-			context.Validated(identity);
-
-		}
 
 		public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context) {
 
@@ -76,6 +57,45 @@ namespace OAuthPrototype.API.Providers {
 			context.OwinContext.Set("as:clientRefreshTokenLifeTime", client.RefreshTokenLifeTime.ToString());
 
 			context.Validated();
+			return Task.FromResult<object>(null);
+		}
+
+
+		public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context) {
+
+			string allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin") ?? "*";
+
+			context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+			using (AuthRepository repo = new AuthRepository()) {
+				IdentityUser user = await repo.FindUser(context.UserName, context.Password);
+
+				if (user == null) {
+					context.SetError("invalid_grant", "The user name or password is incorrect.");
+					return;
+				}
+			}
+
+			var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+			identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+			identity.AddClaim(new Claim("sub", context.UserName));
+			identity.AddClaim(new Claim("role", "user"));
+
+			var props = new AuthenticationProperties(new Dictionary<string, string> {
+				{"as:client_id", context.ClientId ?? string.Empty},
+				{"userName", context.UserName}
+			});
+
+			var ticket = new AuthenticationTicket(identity, props);
+			context.Validated(ticket);
+
+		}
+
+		public override Task TokenEndpoint(OAuthTokenEndpointContext context) {
+			foreach (var property in context.Properties.Dictionary) {
+				context.AdditionalResponseParameters.Add(property.Key, property.Value);
+			}
+
 			return Task.FromResult<object>(null);
 		}
 	}
